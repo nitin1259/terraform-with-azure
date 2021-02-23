51e017c427e10179b319628a8c6eaf6ffa4b9d4f
Nitin SINGH <nitinkumar.singh@f5.com>
2021-02-23 23:11:33 +0530

    override variables with tfvars

--- 02_WebApps/variables.tf
+++ 02_WebApps/variables.tf
@@ -1,25 +1,21 @@
 variable "resource_group_name" {
-  default     = "example-resources"
   type        = string
   description = "RG name in azure"
 }
 
 
 variable "resource_group_location" {
-  default     = "West Europe"
   type        = string
   description = "RG location in azure"
 }
 
 variable "app_service_plan_name" {
-  default     = "example-appserviceplan"
   type        = string
   description = "App Service plan name for azure"
 }
 
 
 variable "app_service_name" {
-  default     = "demo-unique-app-service"
   type        = string
   description = "App Service name in Azure"
 }
